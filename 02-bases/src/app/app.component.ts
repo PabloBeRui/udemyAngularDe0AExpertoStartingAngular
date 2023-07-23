219c89ffe89cbf4f896c47273a2c96b08afa6243
PabloBeRui <pablo.bellon.ruibal@gmail.com>
2023-07-23 13:04:05 +0200

    counter component added

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -7,14 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = '02-bases';
-  public counter: number = 16;
-  increaseBy(value: number): void {
-    this.counter += value;
-  }
-  decreaseBy(value: number): void {
-    this.counter -= value;
-  }
-  counterReset(value: number): void {
-    this.counter = value;
-  }
+  
 }
--- 02-bases/src/app/counter/counter.component.ts
+++ 02-bases/src/app/counter/counter.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola Componente</h1>',
+  template: `<h3>contador: {{ counter }}</h3>
+    <button (click)="decreaseBy(1)">-1</button>
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="counterReset(16)">Reset</button>`,
 })
-export class CounterComponent {}
+export class CounterComponent {
+  public counter: number = 16;
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+  decreaseBy(value: number): void {
+    this.counter -= value;
+  }
+  counterReset(value: number): void {
+    this.counter = value;
+  }
+  constructor() {}
+}
