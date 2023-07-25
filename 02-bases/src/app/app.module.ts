94d83da304ea0236d6da511380f0304a10d846ec
PabloBeRui <pablo.bellon.ruibal@gmail.com>
2023-07-25 19:43:13 +0200

    Learning modules 3

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
 import { AppComponent } from './app.component';
 
 // import { HeroComponent } from './heroes/hero/hero.component';
 // import { ListComponent } from './heroes/list/list.component';
 import { CounterModule } from './counter/counter.module';
 import { HeroModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CounterModule, HeroModule],
+  imports: [BrowserModule, CounterModule, HeroModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
- 
