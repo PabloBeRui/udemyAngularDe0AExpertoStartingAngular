1c5016cfde29c39e7ad634f4a8377960d11da2b9
PabloBeRui <pablo.bellon.ruibal@gmail.com>
2023-07-26 19:03:26 +0200

    learning [ngModel]

--- 02-bases/src/app/dbz/components/addCharacter/addCharacter.component.ts
+++ 02-bases/src/app/dbz/components/addCharacter/addCharacter.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-character',
   templateUrl: './addCharacter.component.html',
   styleUrls: ['./addCharacter.component.css'],
 })
-export class AddCharacterComponent {}
+export class AddCharacterComponent {
+  public character: Character = {
+    name: '',
+    power: 0,
+  };
+  emitCharacter():void {
+    console.log(this.character);
+
+  }
+}
